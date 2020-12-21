a3d56e48657674fe3a626ab38b66c12e20ba6cfe
Maciej Sobieszuk <maciek.sobieszuk@gmail.com>
2020-12-21 03:03:17 +0100

    Added requirements, module5

---
+++ module_5/zad_143/homework_1/main.py
@@ -0,0 +1,16 @@
+#instalacja bibliotek
+
+import geopy
+import requests
+
+
+def run_example():
+    address = "aleja Grunwaldzka 472B, 80-309 Gdańsk"
+    geolocator = geopy.Nominatim(user_agent="webinar-agent")
+    address_code = geolocator.geocode(address)
+    print(address_code.latitude)
+    print(address_code.longitude)
+
+
+if __name__ == "__main__":
+    run_example()
